4a731256abebe0bc36e3af252c28eb4283588558
Vitor Ferreira Bem Silva <vfr@certi.org.br>
2020-11-07 22:21:30 -0300

    v2

--- js/stage-1.js
+++ js/stage-1.js
@@ -3,73 +3,89 @@ var ctx = canvas.getContext('2d');
 
 soundtrack_playing = false;
 
-var score = 0;
+var score = 100;
 var goal = 100;
 
 var frame_to_second_count = 0;
-var timer = 120;
+var timer = 180;
 
-var current_letter_objective = "A";
+var current_letter_objective = 0;
 
-var next_spawn_letter = "A";
+var next_spawn_letter = 1;
 
+var height_player = 60;
+var width_player = 60;
+var hitbox_player_x = 38;
+var hitbox_player_y = 28;
 var x_player = 360;
 var y_player = 300;
 
 var height_letter = 30;
 var width_letter = 30;
+var hitbox_letter_x = 21;
+var hitbox_letter_y = 21;
 
-var x_letter_a = 150;
-var y_letter_a = 180;
+var letter_text_1 = getObjectiveLetter(current_letter_objective);
+var letter_color_1 = "magenta";
+var x_letter_1 = 150;
+var y_letter_1 = 180;
 
-var x_letter_b = 380;
-var y_letter_b = 180;
+var letter_text_2 = getRandomLetter();
+var letter_color_2 = "red";
+var x_letter_2 = 380;
+var y_letter_2 = 180;
 
-var x_letter_c = 600;
-var y_letter_c = 180;
+var letter_text_3 = getRandomLetter();
+var letter_color_3 = "yellow";
+var x_letter_3 = 600;
+var y_letter_3 = 180;
 
-var height_player = 60;
-var width_player = 60;
+
 var speed_player = 1;
 
 var sprite_player = new Image();
-sprite_player.src = './assets/plane.png';
+sprite_player.src = './assets/char_right_1.png';
 
-var sprite_letter_a = new Image();
-sprite_letter_a.src = './assets/a.png';
+sprite_animations = ['./assets/char_right_1.png', './assets/char_right_2.png', './assets/char_right_3.png', './assets/char_right_4.png'];
+sprite_animations_right = ['./assets/char_right_1.png', './assets/char_right_2.png', './assets/char_right_3.png', './assets/char_right_4.png'];
+sprite_animations_left = ['./assets/char_left_1.png', './assets/char_left_2.png', './assets/char_left_3.png', './assets/char_left_4.png'];
 
-var sprite_letter_b = new Image();
-sprite_letter_b.src = './assets/b.png';
+current_sprite_animation = 0;
 
-var sprite_letter_c = new Image();
-sprite_letter_c.src = './assets/c.png';
 
 soundtrack = new sound("./assets/stage-1.mp3");
-
-
-requestAnimationFrame(gameloop);
+window.onkeyup = requestAnimationFrame(gameloop);
 
 function gameloop() {
-    
     window.onkeydown = movePlayer;
     window.onkeyup = stopPlayer;
 
+    hitbox_player_position_x = x_player + 13;
+    hitbox_player_position_y = y_player + 20;
+
     spawnPlayer(x_player,y_player);
 
+    hitbox_letter_1_position_x = x_player - 11;
+    hitbox_letter_1_position_y = y_player - 27;
+    spawnLetter1(x_letter_1, y_letter_1);
 
-    spawnLetterA(x_letter_a, y_letter_a);
-    spawnLetterB(x_letter_b, y_letter_b);
-    spawnLetterC(x_letter_c, y_letter_c);
+    hitbox_letter_2_position_x = x_player - 11;
+    hitbox_letter_2_position_y = y_player - 27;
+    spawnLetter2(x_letter_2, y_letter_2);
+
+    hitbox_letter_3_position_x = x_player - 11;
+    hitbox_letter_3_position_y = y_player - 27;
+    spawnLetter3(x_letter_3, y_letter_3);
 
     collisionCheck();
 
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "white";
     ctx.textAlign = "left";
-    ctx.font='bold 12px Arial';
-    ctx.fillText("Score: " + score, 10, 380); 
-    ctx.fillText("Goal: " + goal, 80, 380); 
-    ctx.fillText("Time left: " + Math. floor(timer / 60) + ":" + timer%60, 150, 380); 
-    ctx.fillText("Current Letter: " + current_letter_objective, 250, 380);
+    ctx.font='bold 18px Arcade';
+    ctx.fillText("Pontos: " + score, 10, 380);
+    ctx.fillText("Objetivo: " + goal, 120, 380);
+    ctx.fillText("Tempo: " + Math. floor(timer / 60) + ":" + timer%60, 250, 380);
+    ctx.fillText("Letra  Atual: " + getObjectiveLetter(current_letter_objective), 350, 380);
 
     y_player += 0.15;
     if(frame_to_second_count >= 59) {
@@ -79,6 +95,16 @@ function gameloop() {
         frame_to_second_count++;
     }
 
+    if(frame_to_second_count % 8 == 0) {
+        if(current_sprite_animation < 3){
+            current_sprite_animation++;
+        }
+        else {
+            current_sprite_animation = 0;
+        }
+    }
+
+    sprite_player.src = sprite_animations[current_sprite_animation]
     if(x_player > 800){
         x_player = 1;
     }
@@ -105,29 +131,48 @@ function spawnPlayer(x,y) {
     ctx.drawImage(sprite_player, x, y, height_player, width_player);
 }
 
-function spawnLetterA(x,y) {
-    next_spawn_letter = "B"
-    ctx.drawImage(sprite_letter_a, x, y, height_letter, width_letter);
+function spawnLetter1(x,y) {
+    ctx.fillStyle = letter_color_1;
+    ctx.textAlign = "center";
+    ctx.font='bold 50px Arcade';
+    ctx.strokeStyle = 'black';
+    ctx.fillText(letter_text_1, x, y);
+    ctx.strokeText(letter_text_1, x, y);
+    next_spawn_letter = 2;
 }
 
-function spawnLetterB(x,y) {
-    next_spawn_letter = "C"
-    ctx.drawImage(sprite_letter_b, x, y, height_letter, width_letter);
+function spawnLetter2(x,y) {
+    ctx.fillStyle = letter_color_2;
+    ctx.textAlign = "center";
+    ctx.font='bold 50px Arcade';
+    ctx.strokeStyle = 'black';
+    ctx.fillText(letter_text_2, x, y);
+    ctx.strokeText(letter_text_2, x, y);
+    next_spawn_letter = 3;
 }
 
-function spawnLetterC(x,y) {
-    next_spawn_letter = "A"
-    ctx.drawImage(sprite_letter_c, x, y, height_letter, width_letter);
+function spawnLetter3(x,y) {
+    ctx.fillStyle = letter_color_3;
+    ctx.textAlign = "center";
+    ctx.font='bold 50px Arcade';
+    ctx.strokeStyle = 'black';
+    ctx.fillText(letter_text_3, x, y);
+    ctx.strokeText(letter_text_3, x, y);
+    next_spawn_letter = 1
 }
 
 function generateLetterY() {
     do {
         new_y = Math.random() * (300 - 40) + 40;
     }
-        while (new_y == y_letter_a
-            || new_y == y_letter_b
-            || new_y == y_letter_c
-            || new_y == y_player);
+        while (new_y == y_letter_1
+            || new_y == y_letter_2
+            || new_y == y_letter_3
+            || new_y == y_player
+            || (new_y > y_letter_1 && new_y < y_letter_1)
+            || (new_y < y_letter_2 && new_y > y_letter_2)
+            || (new_y < y_letter_3 && new_y > y_letter_3)
+            || (new_y < y_player && new_y > y_player));
     return new_y;
 }
 
@@ -135,10 +180,14 @@ function generateLetterX() {
     do {
         new_x = Math.random() * (600 - 40) + 40;
     }
-    while (new_x == x_letter_a
-        || new_x == x_letter_b
-        || new_x == x_letter_c
-        || new_x == x_player);
+    while (new_x == x_letter_1
+        || new_x == x_letter_2
+        || new_x == x_letter_3
+        || new_x == x_player
+        || (new_x < x_letter_1 && new_x > x_letter_1)
+        || (new_x > x_letter_2 && new_x < x_letter_2)
+        || (new_x > x_letter_3 && new_x < x_letter_3)
+        || (new_x > x_player - 50 && new_x < x_player + 50));
     return new_x;
 }
 
@@ -146,36 +195,24 @@ function movePlayer(keyPress){
     if(soundtrack_playing == false){
         soundtrack.play();
         soundtrack_playing = true;
-        console.log("sound node");
     }
-    speed_player++;
-    if(keyPress.keyCode == 87  ) {
+    speed_player+=1;
+    if(speed_player > 10){
+        speed_player = 10;
+    }
+    if(keyPress.keyCode == 38  ) {
         y_player = y_player - speed_player;
     }
-    if(keyPress.keyCode == 83  ) {
+    if(keyPress.keyCode == 40  ) {
         y_player = y_player + speed_player;
     }
-    if(keyPress.keyCode == 68  ) {
+    if(keyPress.keyCode == 39  ) {
+        sprite_animations = sprite_animations_right;
         x_player = x_player + speed_player;
     }
-    if(keyPress.keyCode == 65  ) {
+    if(keyPress.keyCode == 37  ) {
+        sprite_animations = sprite_animations_left;
         x_player = x_player - speed_player;
-    }
-    if(keyPress.keyCode == 81  ) {
-        x_player = x_player - speed_player;
-        y_player = y_player - speed_player;
-    }
-    if(keyPress.keyCode == 67  ) {
-        x_player = x_player + speed_player;
-        y_player = y_player + speed_player;
-    }
-    if(keyPress.keyCode == 69  ) {
-        x_player = x_player + speed_player;
-        y_player = y_player - speed_player;
-    }
-    if(keyPress.keyCode == 90  ) {
-        x_player = x_player - speed_player;
-        y_player = y_player + speed_player;
     }
 }
 
@@ -185,33 +222,17 @@ function stopPlayer(keyPress){
     }
     while(speed_player > 1){
         speed_player--;
-        if(keyPress.keyCode == 87  ) {
+        if(keyPress.keyCode == 38) {
             y_player = y_player - speed_player;
         }
-        if(keyPress.keyCode == 83  ) {
+        if(keyPress.keyCode == 40) {
             y_player = y_player + speed_player;
         }
-        if(keyPress.keyCode == 68  ) {
+        if(keyPress.keyCode == 39) {
             x_player = x_player + speed_player;
         }
-        if(keyPress.keyCode == 65  ) {
+        if(keyPress.keyCode == 37) {
             x_player = x_player - speed_player;
-        }
-        if(keyPress.keyCode == 81  ) {
-            x_player = x_player - speed_player;
-            y_player = y_player - speed_player;
-        }
-        if(keyPress.keyCode == 67  ) {
-            x_player = x_player + speed_player;
-            y_player = y_player + speed_player;
-        }
-        if(keyPress.keyCode == 69  ) {
-            x_player = x_player + speed_player;
-            y_player = y_player - speed_player;
-        }
-        if(keyPress.keyCode == 90  ) {
-            x_player = x_player - speed_player;
-            y_player = y_player + speed_player;
         }
     if(speed_player <= 1){
         speed_player = 1;
@@ -220,83 +241,71 @@ function stopPlayer(keyPress){
 }
 
 function collisionCheck() {
-    if(((x_letter_a + width_letter - 10) > x_player
-        && x_letter_a < (x_player + width_player))
-        && ((y_letter_a + height_letter - 10) > y_player
-        && y_letter_a < (y_player + height_player)))
+    if(((x_letter_1 + width_letter - 10) > x_player
+        && x_letter_1 < (x_player + width_player))
+        && ((y_letter_1 + height_letter - 20) > y_player + 20
+        && y_letter_1 < (y_player + height_player)))
       {
-        check_validity("A");
+        check_validity(1);
       }
-    if(((x_letter_b + width_letter - 10) > x_player
-        && x_letter_b < (x_player + width_player))
-        && ((y_letter_b + height_letter - 10) > y_player
-        && y_letter_b < (y_player + height_player)))
+    if(((x_letter_2 + width_letter - 10) > x_player
+        && x_letter_2 < (x_player + width_player))
+        && ((y_letter_2 + height_letter - 20) > y_player + 20
+        && y_letter_2 < (y_player + height_player)))
       {
-        check_validity("B");
+        check_validity(2);
       }
-    if(((x_letter_c + width_letter - 10) > x_player 
-        && x_letter_c < (x_player + width_player)) 
-        && ((y_letter_c + height_letter - 10) > y_player 
-        && y_letter_c < (y_player + height_player))) 
+    if(((x_letter_3 + width_letter - 10) > x_player 
+        && x_letter_3 < (x_player + width_player)) 
+        && ((y_letter_3 + height_letter - 20) > y_player + 20
+        && y_letter_3 < (y_player + height_player))) 
       {
-        check_validity("C");
+        check_validity(3);
       }
 }
 
 function check_validity(collision){
-    if( collision == current_letter_objective){
+    x_letter_1 = generateLetterX();
+    y_letter_1 = generateLetterY();
+    x_letter_2 = generateLetterX();
+    y_letter_2 = generateLetterY();
+    x_letter_3 = generateLetterX();
+    y_letter_3 = generateLetterY();
+
+    if( collision == 1){
         score += 10;
         correct = new sound("./assets/correct.wav");
         correct.play();
-        if(current_letter_objective == "A"){
-            x_letter_a = generateLetterX();
-            y_letter_a = generateLetterY();
-            current_letter_objective = "B";
-        }
-        else if(current_letter_objective == "B"){
-            x_letter_b = generateLetterX();
-            y_letter_b = generateLetterY();
-            current_letter_objective = "C";
-        }
-        else if(current_letter_objective == "C"){
-            x_letter_c = generateLetterX();
-            y_letter_c = generateLetterY();
-            current_letter_objective = "A";
-        }
+        current_letter_objective ++;
     } else {
         score -= 10;
         wrong = new sound("./assets/wrong.wav");
         wrong.play();
-        if(collision == "A"){
-            x_letter_a = generateLetterX();
-            y_letter_a = generateLetterY();
-        }
-        if(collision == "B"){
-            x_letter_b = generateLetterX();
-            y_letter_b = generateLetterY();
-        }
-        if(collision == "C"){
-            x_letter_c = generateLetterX();
-            y_letter_c = generateLetterY();
-        }
     }
-    console.log(score);
+    letter_color_1 = getRandomColor();
+    letter_color_2 = getRandomColor();
+    letter_color_3 = getRandomColor();
+
+    letter_text_1 = getObjectiveLetter(current_letter_objective);
+    letter_text_2 = getRandomLetter();
+    letter_text_3 = getRandomLetter();
 }
 
 function timeUp(){
-    ctx.textAlign = "left";
-    
+    ctx.textAlign = "center";
+    ctx.fillStyle = "white";
+    ctx.font='24px Arcade';
     if(score >= goal){
         victory = new sound("./assets/victory.wav");
         victory.play();
-        ctx.fillText("You win! Go to next stage! Score: " + score, canvas.width/2, canvas.height/2);
+        ctx.fillText("Voce venceu! Va para a proxima fase: " + score, canvas.width-400, canvas.height-200);
         nextStageButton();
     } else {
         defeat = new sound("./assets/defeat.wav");
         defeat.play();
-        ctx.fillText("Not enough Point! Try Again! Score: " + score, canvas.width/2, canvas.height/2);
+        ctx.fillText("Voce nao conseguiu pontos o suficiente! Tente novamente: " + score, canvas.width-400, canvas.height-200);
     }
-    
+
     clearTimeout();
 
 }
@@ -306,9 +315,10 @@ function outOfBounds(){
     explosion.play();
     defeat = new sound("./assets/defeat.wav");
     defeat.play();
-    ctx.fillStyle = "black";
-    ctx.textAlign = "left";
-    ctx.fillText("You blew up! Game Over! Score: " + score, canvas.width/2, canvas.height/2); 
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.font='24px Arcade';
+    ctx.fillText("Oh nao, voce caiu no chao! Tente de novo: " + score, canvas.width-400, canvas.height-200); 
     clearTimeout();
 }
 
@@ -332,4 +342,37 @@ function sound(src) {
     this.stop = function(){
         this.sound.pause();
     }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomLetter() {
+    alphabet = getAlphabet();
+    letter = getRandomInt(0, 25);
+    while(letter == current_letter_objective){
+        letter = getRandomInt(0, 25);
+    }
+    return alphabet[letter];
+}
+
+function getRandomColor() {
+    colors = ["magenta","red","blue","green","yellow","pink","lightblue", "lightred", "purple", "brown", "white"]
+    return colors[getRandomInt(0,10)]
+}
+
+function getObjectiveLetter(number) {
+    if(current_letter_objective > 25){
+        current_letter_objective = 0;
+        number = 0;
+    }
+    alphabet = getAlphabet();
+    return alphabet[number];
+}
+
+function getAlphabet() {
+    return "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 }
